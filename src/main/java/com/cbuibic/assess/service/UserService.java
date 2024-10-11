d590de928e890bc796dced05596196a091c97ecd
David Lungu <lungudjos@gmail.com>
2024-10-11 11:20:40 +0200

    working on the business logic

--- src/main/java/com/cbuibic/assess/service/UserService.java
+++ src/main/java/com/cbuibic/assess/service/UserService.java
@@ -1,4 +1,14 @@
 package com.cbuibic.assess.service;
 
+import com.cbuibic.assess.dto.UserDto;
+import com.cbuibic.assess.model.User;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
 public interface UserService {
+    User creatNewUser(UserDto userDto);
+    void deleteUser (User user);
+    Optional<User> findUserById (Long id);
 }
--- src/main/java/com/cbuibic/assess/service/UserServiceImpl.java
+++ src/main/java/com/cbuibic/assess/service/UserServiceImpl.java
@@ -1,4 +1,67 @@
 package com.cbuibic.assess.service;
 
+import com.cbuibic.assess.dto.UserDto;
+import com.cbuibic.assess.model.User;
+import com.cbuibic.assess.repostitory.UserRepo;
+import org.hibernate.ObjectNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.nio.file.attribute.UserPrincipalNotFoundException;
+import java.util.Optional;
+
 public class UserServiceImpl implements UserService{
+
+    @Autowired
+    private UserRepo userRepo;
+
+    // the logic
+    @Override
+    public User creatNewUser(UserDto userDto) {
+        var user = new User();
+        // validating the new user
+        User user1 = userRepo.findUserByEmail(userDto.getEmail());
+        if(user1 == null) {
+            throw new ObjectNotFoundException(userDto, userDto.getEmail());
+        } else  {
+
+            // creating new user
+            user.setFirstName(userDto.getFirstName());
+            user.setLastName(userDto.getLastName());
+            user.setEmail(userDto.getEmail());
+            user.setPassword(userDto.getPassword());
+            user.setPhone(userDto.getPhone());
+            user.setEmpId(userDto.getEmpId());
+
+
+            userRepo.save(user);
+        }
+
+        return null;
+    }
+
+    /**
+     * delete user
+     * @param user
+     */
+    @Override
+    public void deleteUser(User user) {
+        // validating the user before delete
+        Optional<User> user1 = userRepo.findById(user.getId());
+        if(user1 == null){
+            throw new ObjectNotFoundException(user, user.getEmail());
+        } else{
+            userRepo.delete(user);
+        }
+
+    }
+
+    /**
+     * find user by id
+     * @param id
+     * @return
+     */
+    @Override
+    public Optional<User> findUserById(Long id) {
+        return userRepo.findById(id);
+    }
 }
