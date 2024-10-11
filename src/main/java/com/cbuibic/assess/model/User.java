c7541842282cbeef6fc92699b01d178a891cec33
David Lungu <lungudjos@gmail.com>
2024-10-11 10:26:12 +0200

    adding the user model

---
+++ src/main/java/com/cbuibic/assess/model/User.java
@@ -0,0 +1,19 @@
+package com.cbuibic.assess.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String password;
+    private String phone;
+    private String empId;
+}
