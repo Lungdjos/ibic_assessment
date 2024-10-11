1bbc4e76090e85112cd7f0031501d9a3a19ad509
David Lungu <lungudjos@gmail.com>
2024-10-11 11:20:15 +0200

    working on the controllers

--- src/main/java/com/cbuibic/assess/controller/MainContl.java
+++ src/main/java/com/cbuibic/assess/controller/MainContl.java
@@ -1,4 +1,17 @@
 package com.cbuibic.assess.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class MainContl {
+    @GetMapping("/login")
+    public String login() {
+        return "login";
+    }
+
+    @GetMapping("/")
+    public String home() {
+        return "index";
+    }
 }
---
+++ src/main/java/com/cbuibic/assess/controller/UserContl.java
@@ -0,0 +1,34 @@
+package com.cbuibic.assess.controller;
+
+import com.cbuibic.assess.dto.UserDto;
+import com.cbuibic.assess.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/registration")
+public class UserContl {
+
+    @Autowired
+    private UserService userService;
+
+    @ModelAttribute("user")
+    public UserDto userDto() {
+        return new UserDto();
+    }
+
+    @GetMapping
+    public String showRegistrationForm() {
+        return "registration";
+    }
+
+    @PostMapping
+    public String registerUserAccount(@ModelAttribute("user") UserDto registrationDto) {
+        userService.creatNewUser(registrationDto);
+        return "redirect:/registration?success";
+    }
+}
