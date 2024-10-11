c1edae53725c26a71c47f2da14020cf371e41a56
David Lungu <lungudjos@gmail.com>
2024-10-11 13:38:19 +0200

    linking the home page

--- src/main/java/com/cbuibic/assess/controller/MainContl.java
+++ src/main/java/com/cbuibic/assess/controller/MainContl.java
@@ -1,10 +1,18 @@
 package com.cbuibic.assess.controller;
 
+import ch.qos.logback.core.model.Model;
+import com.cbuibic.assess.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class MainContl {
+
+    @Autowired
+    private UserService userService;
+
     @GetMapping("/login")
     public String login() {
         return "login";
@@ -13,5 +21,14 @@ public class MainContl {
     @GetMapping("/")
     public String home() {
         return "index";
+    }
+
+
+    // showing the users
+    @GetMapping("/index")
+    public String showUserList(ModelMap model) {
+        model.addAttribute("users", userService.getAllUsers());
+        return "index";
+
     }
 }
